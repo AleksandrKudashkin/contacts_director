be3a7a8fe0adfd7b83aaf7a95d28b974685ca80b
AleksandrKudashkin <alex@kudashkin.pro>
2017-06-11 21:43:47 -0400

    Add Reference to User from a Contact

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170612013147) do
+ActiveRecord::Schema.define(version: 20170612014221) do
 
   create_table "addresses", force: :cascade do |t|
     t.string "street_1"
@@ -40,7 +40,9 @@ ActiveRecord::Schema.define(version: 20170612013147) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "category_id"
+    t.integer "user_id"
     t.index ["category_id"], name: "index_contacts_on_category_id"
+    t.index ["user_id"], name: "index_contacts_on_user_id"
   end
 
   create_table "emails", force: :cascade do |t|
