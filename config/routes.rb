99236270f9be4ecc5f4a478a193e155837d958bd
AleksandrKudashkin <alex@kudashkin.pro>
2017-06-11 23:42:04 -0400

    Add sign_up, sign_out functions

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
 
   resources :contacts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'contacts#index'
+  root 'welcome#index'
 end
---
+++ spec/features/sign_up_spec.rb
@@ -0,0 +1,19 @@
+require_relative 'feature_helper'
+
+feature 'User signs up', '
+  In order to be able to manage my Contacts
+  As an User
+  I want to be able to sign up to site
+' do
+  background do
+    visit new_user_registration_path
+    fill_in 'Email', with: 'iam@theuser.com'
+    fill_in 'Password', with: '12345678'
+    fill_in 'Password confirmation', with: '12345678'
+    click_on 'Sign up'
+  end
+
+  scenario 'New user sees the confirmation', :email do
+    expect(current_page).to have_content 'Welcome! You have signed up successfully.'
+  end
+end
